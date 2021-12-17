c372c8c4970e84279d6d317767965c4a34835796
AnilBameta <71186424+AnilBameta@users.noreply.github.com>
2021-12-17 19:18:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Movies from './Movies';
-
 ReactDOM.render(
-  <Movies />,
+  <>
+  <App />
+  </>,
   document.getElementById('root')
 );
 
@@ -14,4 +14,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
