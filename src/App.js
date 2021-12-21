faed3d48dfe4fa4025b2ed796dd170290144759b
AnilBameta <71186424+AnilBameta@users.noreply.github.com>
2021-12-21 14:36:30 +0530

    Update 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
 import Search from './Search';
+import Popup from './Popup';
+import {
+  BrowserRouter as Router,
+  Switch, 
+  Route
+} from 'react-router-dom';
 function App() {
   return (
-    <Search />
+    <Router>
+    <Switch>
+    <route path='/' component={Search} />
+    <Route path='/Popup/:id' component={Popup} />
+    <Switch>
+    <Router>
   );
 }
 
